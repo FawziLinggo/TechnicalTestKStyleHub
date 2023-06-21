a1087ccba3e52c41fe25604444d401d68fd8e9dd
FawziLinggo <fawzilinggo@gmail.com>
2023-06-21 16:22:07 +0700

    Fixing Response GetProductByID

--- domain/presenters/product.go
+++ domain/presenters/product.go
@@ -7,13 +7,9 @@ type GetProductResponse struct {
 }
 
 type GetProductByIDResponse struct {
-	ID               string   `json:"id" example:"c836c745-e1db-4777-9699-e40fffaa302d"`
-	Username         []string `json:"username" example:"fawzi"`
-	Gender           []string `json:"gender" example:"male"`
-	SkinType         []string `json:"skin_type" example:"oily"`
-	SkinColor        []string `json:"skin_color" example:"light"`
-	DescReview       []string `json:"desc_review" example:"Exelent"`
-	JumlahLikeReview int      `json:"jumlah_like_review" example:"100"`
+	ID               string       `json:"id" example:"c836c745-e1db-4777-9699-e40fffaa302d"`
+	DataReviews      []DataReview `json:"data_reviews"`
+	TotalLikeReviews int          `json:"total_like_review" example:"100"`
 }
 
 type GetProductReviewResponse struct {
@@ -21,4 +17,13 @@ type GetProductReviewResponse struct {
 	ProductID  string `json:"product_id" example:"c836c745-e1db-4777-9699-e40fffaa302d"`
 	MemberID   string `json:"member_id" example:"c836c745-e1db-4777-9699-e40fffaa302d"`
 	DescReview string `json:"desc_review" example:"Product 1"`
+}
+
+type DataReview struct {
+	Username   string `json:"username" example:"fawzi"`
+	Gender     string `json:"gender" example:"male"`
+	SkinType   string `json:"skin_type" example:"oily"`
+	SkinColor  string `json:"skin_color" example:"light"`
+	DescReview string `json:"desc_review" example:"It's good"`
+	TotalLike  int    `json:"total_like" example:"1"`
 }
